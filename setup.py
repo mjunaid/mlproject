9fffbf244d981624049cf54620d4371ec45c1457
Junaid Arshad <mjunaidarshad@yahoo.com>
2024-03-27 13:44:20 +0000

    setup.py and requirements initialised

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str) -> List[str]:
+    '''
+    this function takes file name as argument 
+    and returns the list of packages required by the program.
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+    name='Machine Learning Project',
+    version='0.0.1',
+    author='Junaid',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
+
