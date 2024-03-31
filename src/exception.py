56f3a85e1ecca40c57c664218419f1b7a45df0e2
Junaid Arshad <mjunaidarshad@yahoo.com>
2024-03-31 16:40:33 +0100

    updated logger code to exclude log file name from the path

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from logger import logging
 
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -19,4 +19,12 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
+    
+
+if __name__ == '__main__':
+    try:
+        a = 10 / 0
+    except Exception as e:
+        logging.info("Divide by zero")
+        CustomException(e,sys)
     
